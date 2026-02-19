a791f0e7474608fd155b66320c019bb0c663da33
hakeemoloyin <hakeemoloyin@gmail.com>
2026-02-19 16:18:52 +0100

    Additional Update

--- script.js
+++ script.js
@@ -8,4 +8,3 @@ console.log("The Result of the Multiplication is", multiplyNumba(4,5,7));
 console.log("The Result of the Multiplication is", multiplyNumba(8,2,4));
 console.log("The Result of the Multiplication is", multiplyNumba(9,5,9));
 console.log("The Result of the Multiplication is", multiplyNumba(3,3,3));
-updqte
