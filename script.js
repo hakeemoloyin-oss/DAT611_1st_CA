311fd68cd77a1b5c64ca651224123c1f2486ae12
hakeemoloyin <hakeemoloyin@gmail.com>
2026-02-19 15:16:28 +0100

    Multiplication Javascript Assignment

---
+++ script.js
@@ -0,0 +1,9 @@
+function multiplyNumba (...arguments) 
+{
+    if (arguments.length === 0) return 0;
+    return arguments.reduce((multiply, arg) => multiply * arg, 1);
+}
+
+console.log("The Result of the Multiplication is", multiplyNumba(4,5,7));
+console.log("The Result of the Multiplication is", multiplyNumba(1,2,9));
+console.log("The Result of the Multiplication is", multiplyNumba(9,5,8));
